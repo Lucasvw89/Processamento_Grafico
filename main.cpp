85c6cb3a46b6a7ece0132af0c33d9df7062e03d9
italown <italo.leandro25@gmail.com>
2024-05-17 23:37:51 -0300

    Refatoração

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <string>
 #include "src/Camera.cpp"
 #include "src/Plane.cpp"
 #include "src/Vector.cpp"
@@ -8,38 +10,70 @@
 
 using namespace std;
 
+struct CameraConfig {
+    string point_of_origin;
+    string target;
+    string up_vector;
+    string focal_distance;
+    vector<string> objects;
+};
+
 int main() {
-    //IMAGEM
+
+
+
+    IMAGEM
     
     std::vector<object*> objetos;
 
-    //Sistema (x,z,y)
-
-    point p_esfera(0,0,0);
     double r_esfera = 0.5;
-    vetor v1(0, 0, 1);
-    vetor cor (1, 0, 0);
-    vetor v2(0, 0, 1);
-    // std::cout << v1.norma() << std::endl;
-    point p1(1,1,-1);
-    point p2(0,0,0);
-    point p3(1,0,0);
+    
+    point p_esfera1(2,0,0);
+    vetor cor1(1, 0, 0); // vermelho
 
-    point ponto_plano(1,-1,0);
-    vetor normal_plano(0, 1, 0);
+    point p_esfera2(5,0,0);
+    vetor cor2(0, 1, 0); // verde
+
+    point p_esfera3(8,0,0);
+    vetor cor3(0, 0, 1); // azul
+
+    point p_esfera4(-1,0,0);
+    vetor cor4(1, 1, 0); // amarelo
+
+    point p_esfera5(0,-1,0);
+    vetor cor5(0, 1, 1); // ciano
+
+    point p_esfera6(0,0,-1);
+    vetor cor6(1, 0, 1); // rosa
+
+    //CAMERA
+    point pos_cam(0,0,0);
+    point target_cam(1,0.3,0);
+    vetor up_cam(0,1,0);
+
+    point ponto_plano(1,0,0);
+    vetor normal_plano(1, 0, 0);
     
 
     vetor cor_plano(0, 0, 1);
 
-    sphere esfera(p_esfera, 0.5, cor);
-    sphere esfera1(p1, 0.5, cor);
+    sphere esfera1(p_esfera1, 0.5, cor1);
+    sphere esfera2(p_esfera2, 2, cor2);
+    sphere esfera3(p_esfera3, 4, cor3);
+    sphere esfera4(p_esfera4, r_esfera, cor4);
+    sphere esfera5(p_esfera5, r_esfera, cor5);
+    sphere esfera6(p_esfera6, r_esfera, cor6);
+
     plane plano(ponto_plano, normal_plano, cor_plano);
 
-    objetos.push_back(&esfera);
     objetos.push_back(&esfera1);
-    objetos.push_back(&plano);
+    objetos.push_back(&esfera2);
+    objetos.push_back(&esfera3);
+    objetos.push_back(&esfera4);
+    objetos.push_back(&esfera5);
+    objetos.push_back(&esfera6);
 
-    camera cam(400, p2, p3, v1);
+    camera cam(400, pos_cam, target_cam, up_cam);
     cam.render(objetos);
 
 
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -13,13 +13,12 @@ using namespace std;
 class camera 
 {
 private:
-
     point position;
     point target;
     vetor up;
-    vetor W;
-    vetor V;
-    vetor U;
+    vetor W; // Vetor direção da câmera
+    vetor V; // Vetor vertical da câmera
+    vetor U; // Vetor horizontal da câmera
     double distance;
     int height;
     int width;
@@ -27,45 +26,47 @@ private:
     double focal_length;
     double viewport_height;
 
-
 public:
+    point getPosition() const { return position; }
+    point getTarget() const { return target; }
+    vetor getUp() const { return up; }
+    vetor getW() const { return W; }
+    vetor getV() const { return V; }
+    vetor getU() const { return U; }
+    double getDistance() const { return distance; }
+    int getHeight() const { return height; }
+    int getWidth() const { return width; }
+    double getAspectRatio() const { return aspect_ratio; }
+    double getFocalLength() const { return focal_length; }
+    double getViewportHeight() const { return viewport_height; }
 
-        point getPosition() const { return position; }
-        point getTarget() const { return target; }
-        vetor getUp() const { return up; }
-        vetor getW() const { return W; }
-        vetor getV() const { return V; }
-        vetor getU() const { return U; }
-        double getDistance() const { return distance; }
-        int getHeight() const { return height; }
-        int getWidth() const { return width; }
-        double getAspectRatio() const { return aspect_ratio; }
-        double getFocalLength() const { return focal_length; }
-        double getViewportHeight() const { return viewport_height; }
     camera (
         int width,
         point p,
         point t,
         vetor u,
-        double d = 1.0,
+        //double d = 1.0,
         double aspect_ratio = 16.0 / 9.0,
-        double focal_length=0.2,
+        double focal_length = 1,
         double viewport_height = 2.0
     ) {
         this->position = p;
-        this->width= width;
+        this->width = width;
         this->target = t;
-        this->up = u;
-        this->W = (target - position).normalizar();
-        this->V = up.produto_vetorial(W).normalizar();
-        this->U = up.produto_vetorial(V).normalizar();
-        this->distance = d;
+        this->up = u.normalizar();
+        //this->distance = d;
         this->aspect_ratio = aspect_ratio;
         this->focal_length = focal_length;
-        this->viewport_height= viewport_height;
-    };
-    camera(){
+        this->viewport_height = viewport_height;
+
+        // Calcula os vetores ortonormais da câmera
+        this->W = (target - position).normalizar();
+        this->U = up.produto_vetorial(W).normalizar();
+        this->V = W.produto_vetorial(U).normalizar();
     }
+
+    camera() {}
+
     void print() {
         cout << "Camera:" << endl;
         position.print();
@@ -76,51 +77,52 @@ public:
         U.print();
     }
 
-    double ray_color( ray& r, object& s) {
+    double ray_color(ray& r, object& s) {
         double t = s.intersect(r);
         if (t > 0.0) {
             return t;
         }
-        vetor unit_direction = r.direction.normalizar();
-        auto a = 0.5*(unit_direction.getY() + 1.0);
         return INFINITY;
     }
 
-
-    void render(vector<object *> objetos) 
-    {
-
+    void render(vector<object *> objetos) {
         int image_width = this->width;
         int image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+        
+        double viewport_width = viewport_height * aspect_ratio;
+        point camera_center = this->position;
 
-        auto viewport_width = viewport_height * (double(image_width)/image_height);
-        auto camera_center = this->position;
+        // Vetores que apontam as extremidades da tela
+        vetor viewport_u = U * viewport_width;
+        vetor viewport_v = V * (viewport_height * -1);
 
-        auto viewport_u = vetor(viewport_width, 0, 0);
-        auto viewport_v = vetor(0, -viewport_height, 0);
+        // Vetores usados para calcular a posição do próximo pixel (ponto+(vetor*quantidade de pixels))
+        vetor pixel_delta_u = viewport_u / image_width;
+        vetor pixel_delta_v = viewport_v / image_height;
 
-        auto pixel_delta_u = viewport_u / image_width;
-        auto pixel_delta_v = viewport_v / image_height;
-        auto viewport_upper_left = camera_center- vetor(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-        auto pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)*0.5;
+        // Ponto superior esquerdo do pixel inicial
+        point viewport_upper_left = camera_center + W * focal_length - viewport_u / 2 - viewport_v / 2;
+
+        // Calculando o ponto central do pixel
+        point pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
         std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rLinhas restantes: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (pixel_delta_u*i) + (pixel_delta_v*j);
+                auto pixel_center = pixel00_loc + pixel_delta_u * i + pixel_delta_v * j;
                 auto ray_direction = pixel_center - camera_center;
-                ray r(camera_center, ray_direction);
+                ray r(camera_center, ray_direction.normalizar());
                 
-                tuple <int, double, vetor> pixel_info (0, 0, vetor(0,0,0));
+                tuple<int, double, vetor> pixel_info(0, 0, vetor(0, 0, 0));
 
-                for (int k = 0; k < objetos.size(); k++){
+                for (int k = 0; k < objetos.size(); k++) {
                     double t = ray_color(r, *objetos[k]);
                     
                     if (t != INFINITY) {
-                        if (t < get<1>(pixel_info) || get<1>(pixel_info) == 0){
+                        if (t < get<1>(pixel_info) || get<1>(pixel_info) == 0) {
                             pixel_info = make_tuple(k, t, objetos[k]->getColor());
                         }
                     }
@@ -129,9 +131,7 @@ public:
             }
         }
         std::clog << "\rDone.                 \n";
-
-
-    };
+    }
 };
 
 #endif
